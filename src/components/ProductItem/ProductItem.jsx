f3a6404582cfb93bc089176636d1983eb70f3cde
Carlos <carlosaguinaga01@gmail.com>
2021-10-07 19:34:31 -0500

    feat: + and - in card item

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -6,39 +6,77 @@ import { useHistory } from "react-router";
 const ProductItem = ({ name, description, price, id, img }) => {
   const { state, dispatch } = useContext(ShopCartContext);
 
-  const history = useHistory()
-
+  const history = useHistory();
   const [payload, setPayload] = useState({});
+  const productInCart = state.cart.find((p) => p.id === id);
+
+  const handleAddDispatch = () => {
+    dispatch({ type: "ADD", payload });
+  };
+
+  const handleAddItemDispatch = () => {
+    dispatch({ type: "ADD_UNIT_MORE", payload: { id } });
+  };
+
+  const handleRemoveDispatch = () => {
+    dispatch({ type: "REMOVE", payload: { id, price } });
+  };
+
+  const handleRemoveItemDispatch = () => {
+    if (productInCart.quantity === 1) {
+      // eliminar el producto
+      handleRemoveDispatch();
+    }
+    dispatch({ type: "REMOVE_UNIT", payload: { id } });
+  };
+
+  const handleClickItem = () => {
+    history.push(`/product/${id}`);
+  };
 
   useEffect(() => {
-    setPayload({ name, description, price, id, img, quantity: 1 });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setPayload({
+      name,
+      description,
+      price,
+      id,
+      img,
+      quantity: productInCart ? productInCart.quantity : 1,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const handleDispatch = (payload) => {
-    (dispatch({ type: "ADD", payload }))
-
-  }
-
-  const handleClickItem = (payload) => {
-    history.push(`/product/${id}`)
-  }
 
   return (
     <div className="product-card">
       <div className="image-container">
-      <img variant="top" src={img} onClick={handleClickItem} className="card-image" alt={name}/>
+        <img
+          variant="top"
+          src={img}
+          onClick={handleClickItem}
+          className="card-image"
+          alt={name}
+        />
       </div>
       <div className="card-description">
-        <h2 className="card-title" onClick={handleClickItem}>{name}</h2>
+        <h2 className="card-title" onClick={handleClickItem}>
+          {name}
+        </h2>
         <p>{description}</p>
         <span>${price}</span>
-        <button
-          variant={"primary"}
-          onClick={ () => handleDispatch(payload)}
-        >
-          Add
-        </button>
+
+        {productInCart ? (
+          <div>
+            <button onClick={() => handleRemoveItemDispatch(payload)}>-</button>
+            <span>{productInCart.quantity}</span>
+            <button onClick={() => handleAddItemDispatch(payload)}>+</button>
+          </div>
+        ) : (
+          <button onClick={() => handleAddDispatch(payload)}>Add.</button>
+        )}
+
+        {productInCart && (
+          <p className="item_added">{productInCart.quantity} items added</p>
+        )}
       </div>
     </div>
   );
--- src/context/ShopCartContext.jsx
+++ src/context/ShopCartContext.jsx
@@ -8,11 +8,10 @@ const initialState = {
   cart: [],
 };
 
-const getUpdatedCart = (product, action, state) => {
+const getPlusItemCart = (state, action ) => {
 
-  if (!product) {
-    return [...state.cart, action.payload];
-  }
+  console.log(action)
+  const product = state.cart.find((p) => p.id === action.payload.id)
 
   return state.cart.map((p) => {
     if (p.id === product.id) {
@@ -24,14 +23,33 @@ const getUpdatedCart = (product, action, state) => {
   });
 };
 
+const getRemoveItemCart = (state, action) => {
+  
+  const product = state.cart.find((p) => p.id === action.payload.id);
+
+  return state.cart.map((p) => {
+    if (p.id === product.id) {
+      const obj = { ...p };
+      obj.quantity--;
+      return obj;
+    }
+    return p;
+  });
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD":
-      const product = state.cart.find((p) => p.id === action.payload.id);
       return {
         ...state,
         totalPrice: state.totalPrice + action.payload.price,
-        cart: getUpdatedCart(product, action, state),
+        cart: [...state.cart, action.payload],
+      };
+    case "ADD_UNIT_MORE":
+      return {
+        ...state,
+        totalPrice: state.totalPrice + action.payload.price,
+        cart: getPlusItemCart(state, action),
       };
 
     case "REMOVE":
@@ -40,6 +58,13 @@ const reducer = (state, action) => {
         totalPrice: state.totalPrice - action.payload.price,
         cart: state.cart.filter((product) => product.id !== action.payload.id),
       };
+    case "REMOVE_UNIT":
+      return {
+        ...state,
+        totalPrice: state.totalPrice - action.payload.price,
+        cart: getRemoveItemCart(state, action),
+      };
+
     default:
       return state;
   }
